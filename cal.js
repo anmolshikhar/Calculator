b6502ea5c98aa9a46c2ebf2aba7b037de7c07a80
Anmol Shikhar <anmolshikhar2323@gmail.com>
2025-05-13 14:58:42 +0530

    initial logic

--- cal.js
+++ cal.js
@@ -1,6 +1,24 @@
-let expresstion = document.getElementById("input");
-let btn = document.getElementById("btn");
-btn.addEventListener("click",function calculate(){
-    let result = math.evaluate(expresstion.value);
-    console.log(result);
-})
+// // let expresstion = document.getElementById("input");
+// // let btn = document.getElementById("btn");
+// // btn.addEventListener("click",function calculate(){
+// //     let result = math.evaluate(expresstion.value);
+// //     console.log(result);
+// // })
+//let expresstion = document.getElementById("input");
+// let btns = document.getElementsByClassName("inbtn");
+// let reset = document.getElementById("Rbtn");
+// for (let btn of btns) {
+//     btn.addEventListener("click", function calculate() {
+//         expresstion.value+=btn.value;
+
+//     })
+// }
+let expression = document.getElementById("input");
+let btns = document.getElementsByClassName("inbtn");
+let reset = document.getElementById("Rbtn");
+
+for (let btn of btns) {
+    btn.addEventListener("click", function () {
+        expression.value += btn.value;
+    });
+}
