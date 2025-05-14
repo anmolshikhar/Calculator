44f8eeb9ce432251777454636afd5ebda2329d60
Anmol Shikhar <anmolshikhar2323@gmail.com>
2025-05-14 12:09:04 +0530

    Semi_Final project only footer is not ready

--- cal.js
+++ cal.js
@@ -4,21 +4,25 @@
 // //     let result = math.evaluate(expresstion.value);
 // //     console.log(result);
 // // })
-//let expresstion = document.getElementById("input");
-// let btns = document.getElementsByClassName("inbtn");
-// let reset = document.getElementById("Rbtn");
-// for (let btn of btns) {
-//     btn.addEventListener("click", function calculate() {
-//         expresstion.value+=btn.value;
-
-//     })
-// }
-let expression = document.getElementById("input");
+let expresstion = document.getElementById("input");
 let btns = document.getElementsByClassName("inbtn");
 let reset = document.getElementById("Rbtn");
-
+let eual = document.getElementById("Equal");
+let clr = document.getElementById("clr");
 for (let btn of btns) {
-    btn.addEventListener("click", function () {
-        expression.value += btn.value;
-    });
+    btn.addEventListener("click", function calculate() {
+        expresstion.value+=btn.value;
+
+    })
 }
+reset.addEventListener("click", function reset(){
+    expresstion.value = "";
+})
+eual.addEventListener("click", function calculate() {
+  let result = math.evaluate(expresstion.value);
+    expresstion.value = result;
+})
+clr.addEventListener("click", function clear() {
+    expresstion.value = expresstion.value.slice(0, -1);
+})
+
